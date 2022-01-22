a9bdb5b031362bc693ba34c164de5fe2c41a22b5
rouanis5 <ouanis42@gmail.com>
2022-01-22 23:11:29 +0100

    add correction for answer when you submit

--- Componants/form.php
+++ Componants/form.php
@@ -7,14 +7,25 @@
                     <h3><span><?php echo (++$index ?? 1) ?></span><?php echo $values['question'] ?></h3>
                     <?php $name = is_array($values['réponse']) ? $qst . "[]" : $qst;?>
                     <?php for ($i = 0; $i < count($values['choix']); $i++): ?>
-                        <?php $required = ($i == 0 and $values['type'] != "checkbox") ? "required" : ""?>
+                        <?php $required = ($i == 0 and $values['type'] != "checkbox") ? "required" : "";?>
+<?php
+$id = $qst . "_" . $i;
+$className = "";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (in_array($id, $true_reps) || $true_text[$id]) {
+        $className = "true";
+    } elseif (in_array($id, $false_reps) || $false_text[$id]) {
+        $className = "false";
+    }
+}
+?>
                         <?php if ($values['type'] != "text"): ?>
-                            <div class='answer' id='<?php echo $qst . "_" . $i ?>'>
+                            <div class='answer <?php echo $className ?>' id='<?php echo $id ?>'>
                                 <input type='<?php echo $values['type'] ?>' id='<?php echo "a__$qst$i" ?>' name='<?php echo $name ?>' value='<?php echo $qst . "_" . $i ?>' <?php echo $required ?>>
                                 <label for='<?php echo "a__$qst$i" ?>'><?php echo $values['choix'][$i] ?></label>
                             </div>
                         <?php else: ?>
-                            <input class='answer' type='text' id='<?php echo $qst . "_" . $i ?>' name='<?php echo $name ?>' placeholder='Taper la réponse ici ...'required striptag>
+                            <input class='answer <?php echo $className ?>' type='text' id='<?php echo $id ?>' value='<?php echo $true_text[$id] ?? $false_text[$id] ?? "" ?>' name='<?php echo $name ?>' placeholder='Taper la réponse ici ...'required striptag>
                         <?php endif?>
                     <?php endfor?>
                 </div>
@@ -23,3 +34,11 @@
         </form>
     </div>
 </div>
+<?php if ($_SERVER['REQUEST_METHOD'] == 'POST'): ?>
+<script>
+    document.querySelector(".submit").style.display = "none";
+    document.querySelectorAll("input").forEach(el => {
+        el.disabled = true;
+    });
+</script>
+<?php endif?>
